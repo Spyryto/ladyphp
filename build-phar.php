f25b9249c3fdc88b02d192b1d3cee700408829b1
unu <unumin@gmail.com>
2014-07-20 03:36:14 +0200

    added script that builds phar file

---
+++ build-phar.php
@@ -0,0 +1,16 @@
+#!/usr/bin/env php
+<?php
+chdir(__DIR__);
+$phar = new Phar('ladyphp.phar');
+$phar->buildFromDirectory(__DIR__ . '/src');
+$phar->setStub('#!/usr/bin/env php
+<?php
+Phar::mapPhar("ladyphp.phar");
+require "phar://ladyphp.phar/Lady.php";
+require "phar://ladyphp.phar/LadyCommand.php";
+$command = new LadyCommand();
+$command->run($argv);
+__HALT_COMPILER();');
+$phar->compressFiles(Phar::GZ);
+chmod('ladyphp.phar', 0755);
+echo "File ladyphp.phar created.\n";
