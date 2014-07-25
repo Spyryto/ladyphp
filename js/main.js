7f706b5548e8bb7ad5df7426fbc37d361bdf00b1
unu <unumin@gmail.com>
2014-07-25 04:42:39 +0200

    first commit

---
+++ js/main.js
@@ -0,0 +1,29 @@
+var ladyBox = document.getElementById('lady');
+var phpBox = document.getElementById('php');
+
+var processing = false;
+function convert(toLady) {
+  if (!processing) {
+    processing = true;
+    if (toLady) {
+      ladyBox.value = Lady.toLady(phpBox.value);
+    } else {
+      phpBox.value = Lady.toPhp(ladyBox.value);
+    }
+    processing = false;
+  }
+}
+convert();
+
+ladyBox.onkeyup = function(){ convert(false); };
+phpBox.onkeyup = function(){ convert(true); };
+
+var skip = false;
+ladyBox.onscroll = function() {
+  if (skip){skip=false; return;} else skip=true; 
+  phpBox.scrollTop = ladyBox.scrollTop;
+}
+phpBox.onscroll = function() {
+  ladyBox.scrollTop = phpBox.scrollTop;
+}
+
