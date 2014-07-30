30831326ab42b699962d6b8e143f92b4a014c7fd
unu <unumin@gmail.com>
2014-07-30 02:18:08 +0200

    lighter theme

--- js/main.js
+++ js/main.js
@@ -1,14 +1,26 @@
 var ladyBox = document.getElementById('lady');
 var phpBox = document.getElementById('php');
 
+var skip = false;
+ladyBox.onscroll = function() {
+  if (skip){skip=false; return;} else skip=true;
+  phpBox.scrollTop = ladyBox.scrollTop;
+}
+phpBox.onscroll = function() {
+  ladyBox.scrollTop = phpBox.scrollTop;
+}
+
+
 var processing = false;
 function convert(toLady) {
   if (!processing) {
     processing = true;
     if (toLady) {
       ladyBox.value = Lady.toLady(phpBox.value);
+      phpBox.onscroll();
     } else {
       phpBox.value = Lady.toPhp(ladyBox.value);
+      ladyBox.onscroll();
     }
     processing = false;
   }
@@ -17,13 +29,5 @@ convert(false);
 
 ladyBox.onkeyup = function(){ convert(false); };
 phpBox.onkeyup = function(){ convert(true); };
-
-var skip = false;
-ladyBox.onscroll = function() {
-  if (skip){skip=false; return;} else skip=true; 
-  phpBox.scrollTop = ladyBox.scrollTop;
-}
-phpBox.onscroll = function() {
-  ladyBox.scrollTop = phpBox.scrollTop;
-}
-
+ladyBox.onchange = ladyBox.onkeyup;
+phpBox.onchange = phpBox.onkeyup;
