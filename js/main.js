66b5ea50d15c6bbd0ef42f9f72714db4d59e931f
Honza Novák <unumin@gmail.com>
2014-08-22 20:39:44 +0200

    added google analytics

--- js/main.js
+++ js/main.js
@@ -37,3 +37,10 @@ document.onscroll = function() {
   var top = (window.pageYOffset || document.documentElement.scrollTop);
   titleBox.style.top = (top * 0.2) + 'px';
 };
+
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+ga('create', 'UA-30581306-2', 'honzanovak.com');
+ga('send', 'pageview');
