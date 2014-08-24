4fff1e36f314737c32eeb7f5e6596804896517d7
Honza Novák <unumin@gmail.com>
2014-08-24 19:04:47 +0200

    details

--- js/main.js
+++ js/main.js
@@ -35,12 +35,12 @@ phpBox.onchange = phpBox.onkeyup;
 document.onscroll = function() {
   var titleBox = document.getElementById('title-box');
   var top = (window.pageYOffset || document.documentElement.scrollTop);
-  titleBox.style.top = (top * 0.2) + 'px';
+  titleBox.style.top = (window.innerWidth > 550) ? (top * 0.25) + 'px' : '0';
 };
 
 (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
 (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
 m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
 })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-ga('create', 'UA-30581306-2', 'honzanovak.com');
+ga('create', 'UA-30581306-2', 'auto');
 ga('send', 'pageview');
